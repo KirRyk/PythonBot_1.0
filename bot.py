245bc0d29a42f250d2e2780874b5347fa615ec59
KirRyk <152082924+KirRyk@users.noreply.github.com>
2025-12-07 21:11:14 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -3,7 +3,9 @@ import threading
 from datetime import datetime
 import time
 import pandas
-
+import random
+import requests
+from urllib.parse import quote_plus
 
 BOTTOKEN = "8327175837:AAE_PDIkfU1yOrMoGxx1vRFFcGj_zZ2kVAE"
 
@@ -53,6 +55,19 @@ def cmdInfo(m):
                                 '/unsub - отписаться от уведомлений \n'
                                 '/image - генератор изображений \n'
                                 '/parser - подборка товаров электроники', reply_markup=klava2)
+
+
+
+@bot.message_handler(commands=["image"])
+def sendImg(m):
+    prompt = m.text.partition(' ')[2].strip()
+    bot.send_message(m.chat.id, "Ищу...")
+    seed = random.randint(0, 2_000_000_000)
+    q = quote_plus(f"{prompt}, high quality, very detailed, soft light")
+    url = f"https://image.pollinations.ai/prompt/{q}?width=1920&height=1080&seed={seed}&n=1"
+    res = requests.get(url, timeout=90, allow_redirects=True)
+    bot.send_photo(m.chat.id, res.content)
+
 
 @bot.message_handler(commands=['notice'])
 def cmdNotice(m):
